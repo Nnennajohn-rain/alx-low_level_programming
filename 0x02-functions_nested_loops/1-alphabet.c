cdb3d483103ef59609f54666b5687164498b00a4
Nnenna John <nnennajohn.byu@gmail.com>
2022-09-14 05:59:43 -0700

    Task 1. I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: A function that prints the alphabet:
+ *
+ * Return: 0
+ *
+ */
+
+void print_alphabet(void)
+
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
