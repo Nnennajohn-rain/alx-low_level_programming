7553a4515f0cf95b5eb6ac03618595dedd225869
Nnenna John <nnennajohn.byu@gmail.com>
2022-09-14 21:04:25 -0700

    Task 10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds the intergers
+ *
+ * @a: int to be added to b
+ *
+ * @b: int to be added to a
+ *
+ * Return: returns the result
+ *
+ */
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
