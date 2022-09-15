05e6375dd3e4cfca8230ae2dd42fb0e432683eea
Nnenna John <nnennajohn.byu@gmail.com>
2022-09-14 20:10:03 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times,
+ *
+ * followed by a new  line
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch;
+	
+	i = 0;
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
