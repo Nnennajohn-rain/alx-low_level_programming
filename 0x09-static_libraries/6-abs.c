8a88f6b7b8e28a4b003c1cafab790598dbabb86e
Nnenna John <nnennajohn.byu@gmail.com>
2022-10-03 11:12:35 -0700

    Task 0

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - computes the absolute value of an intege
+ *
+ * @n: the int to check
+ *
+ * Return: the absolute value of n
+ *
+ */
+
+int _abs(int n)
+
+{
+	return (abs(n));
+}
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(void)
+
+{
+	_puts("\"End of the day,my goal is to be D best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
